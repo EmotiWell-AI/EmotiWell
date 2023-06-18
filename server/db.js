b1387010abe59ee15f5912c935053141c0bdb79b
Ronald Arifin <ronaldarifin>
2023-06-18 13:45:05 -0700

    render.com pgpool config

--- server/db.js
+++ server/db.js
@@ -1,12 +1,20 @@
 const Pool = require('pg').Pool;
 
 
+// const pool = new Pool({
+//   user: "postgres",
+//   password: "",
+//   host: "localhost",
+//   port: 5432,
+//   database: "emotiwell"
+// });
+
 const pool = new Pool({
-  user: "postgres",
-  password: "",
-  host: "localhost",
+  user: "queryinfomation_user",
+  password: "h2lIzk6bksDhUdRv3jE9L0o7nIPMctYn",
+  host: "dpg-ci7823p8g3n3vm5u8s50-a",
   port: 5432,
-  database: "emotiwell"
+  database: "queryinfomation"
 });
 
 
