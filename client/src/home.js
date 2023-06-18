8a710cafff39064f6658881612804868060bdd43
sydnee510 <sbsydnee161416@gmail.com>
2023-06-18 13:31:41 -0700

    remove imports

--- client/src/home.js
+++ client/src/home.js
@@ -1,9 +1,7 @@
-import LoginForm from "./auth/login";
 import Chatbot from "./chatbot";
 function Home() {
     return(
         <div>
-              {/* <LoginForm/> */}
               <Chatbot/>
               </div>
         
