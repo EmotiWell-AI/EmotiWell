2ca196635dacb22c995587cd711a0b6b77452c90
sydnee510 <sbsydnee161416@gmail.com>
2023-06-18 10:44:14 -0700

    create and render the homepage

---
+++ client/src/home.js
@@ -0,0 +1,13 @@
+import LoginForm from "./auth/login";
+import Chatbot from "./chatbot";
+function Home() {
+    return(
+        <div>
+              {/* <LoginForm/> */}
+              <Chatbot/>
+              </div>
+        
+    )
+}
+
+export default Home;
